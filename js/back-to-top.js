c7ab1b6fec1c95d1055fef1957d5e2ce167a08e4
Orangesmw <106233500+Orangesmw1@users.noreply.github.com>
2022-10-12 12:46:07 +0700

    done

---
+++ js/back-to-top.js
@@ -0,0 +1,13 @@
+// Get the button
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function topFunction2() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
